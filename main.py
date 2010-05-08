da2b2532a1e9ab899fc8748ba0f293992b5eb6c5
sBlatt <use@github.plz>
2010-05-08 16:20:40 +0200

    Adapted main to have startentries for slideshow applet

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logitech.applets.simple_bg_light.simple_bg_light import SimpleBgLight
 from logitech.applets.simple_display_brightness.simple_display_brightness \
         import SimpleDisplayBrightness
 from logitech.applets.xplanet.xplanet import Xplanet
+## from logitech.applets.simple_slideshow.simple_slideshow import Slideshow
 
 import time
 
@@ -18,8 +19,13 @@ if __name__ == '__main__':
 
         displayBrightness = SimpleDisplayBrightness(lg19)
         lg19.add_applet(displayBrightness)
+
+##      slideshow = Slideshow(lg19, "logitech/applets/simple_slideshow/pics")
+##      lg19.add_applet(slideshow)
+
         while True:
             time.sleep(10)
     finally:
         xplanet.stop()
         lg19.stop_event_handling()
+##	slideshow.stop()
